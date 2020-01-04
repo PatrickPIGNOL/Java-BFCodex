28094c38361c473bd50cf537f01ef806ec5e697c
ppignol <patrick.pignol@gmail.com>
2020-01-04 19:31:55 +0100

    Update BFCodex.java and Main.java

--- src/bf/BFCodex.java
+++ src/bf/BFCodex.java
@@ -1,4 +1,5 @@
 package bf;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,9 +30,9 @@ public class BFCodex
 				vChars.put(vStringChar, vCount);
 				vCount++;
 			}
-		}		
+		}
 		String vResult = "";	
-		int vHalfMin = 16; 
+		int vHalfMin = vMin / 2;
 		int vValue = vMax / vHalfMin;
 		int vReste = vMax % vHalfMin;
 		
@@ -96,9 +97,123 @@ public class BFCodex
 		return vResult;		
 	}
 	
-	public static String BFToText(String pBF)
+	public static String mBFToText(String pBF)
 	{
-		String vResult = "";
+		String vResult = "Message for BF code : " + pBF + "\n";
+		String vErrorMessage = "";
+		int vLoopsCount = 0;
+		List<Integer> vBegins = new ArrayList<Integer>();
+		List<BFLoop> vLoops = new ArrayList<BFLoop>();
+		for(int vIndex = 0; vIndex < pBF.length(); vIndex++)
+		{
+			char vChar = pBF.charAt(vIndex);
+			switch(vChar)
+			{
+				case '[':
+				{
+					vLoopsCount++;
+					vBegins.add(vIndex);
+				}break;
+				case ']':
+				{
+					vLoopsCount--;
+					if(vLoopsCount < 0)
+					{
+						return "Error at " + vIndex + " : closing brace ']' without previous oppening brace '['.";
+					}
+					vLoops.add(new BFLoop(vBegins.get(vBegins.size() - 1), vIndex));
+					vBegins.remove(vBegins.size() - 1);
+				}break;
+			}
+		}
+		if(vLoopsCount > 0)
+		{
+			return "Error missing closing brace ']' for oppening brace at " + vBegins.get(vBegins.size());
+		}
+		int vInstructionPointer = 0;
+		int vMemoryPointer = 0;
+		String vConsole = "";
+		List<Character> vMemory = new ArrayList<Character>();
+		vMemory.add(new Character((char) 0));
+		while(vInstructionPointer < pBF.length())
+		{
+			switch(pBF.charAt(vInstructionPointer))
+			{
+				case '[':
+				{
+					if(vMemory.get(vMemoryPointer) == 0)
+					{
+						for(BFLoop vLoop : vLoops)
+						{
+							if(vLoop.mBegin() == vInstructionPointer)
+							{
+								vInstructionPointer = vLoop.mEnd();
+							}
+						}
+					}
+				}break;
+				case ']':
+				{
+					for(BFLoop vLoop : vLoops)
+					{
+						if(vLoop.mEnd() == vInstructionPointer)
+						{
+							vInstructionPointer = vLoop.mBegin()-1;
+						}
+					}
+				}break;
+				case '>':
+				{
+					vMemoryPointer++;
+					if(vMemoryPointer >= vMemory.size())
+					{
+						vMemory.add(new Character((char)0));
+					}
+				}break;
+				case '<':
+				{
+					vMemoryPointer--;
+					if(vMemoryPointer < 0)
+					{
+						vMemory.add(new Character((char)0));
+						vMemoryPointer++;
+					}
+				}break;
+				case '+':
+				{
+					vMemory.set(vMemoryPointer, (char) (vMemory.get(vMemoryPointer) + 1));
+				}break;
+				case '-':
+				{
+					vMemory.set(vMemoryPointer, (char) (vMemory.get(vMemoryPointer) - 1));
+				}break;
+				case '.':
+				{
+					vConsole += vMemory.get(vMemoryPointer);
+				}break;
+				case ',':
+				{
+					try 
+					{
+						vMemory.set(vMemoryPointer, (char)System.in.read());
+					} 
+					catch (IOException e) 
+					{
+						e.printStackTrace();
+					}
+				}break;
+			}
+			vInstructionPointer++;
+		}
+		vResult += vConsole;
+		vResult += "\n------\nMemory:\n";
+		
+		for(int vIndex = 0; vIndex < vMemory.size(); vIndex++)
+		{
+			vResult += vIndex + ": '";
+			vResult += (char)vMemory.get(vIndex);
+			vResult += "';\n";
+		}
 		return vResult;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,19 @@ public class Main
 {
 	public static void main(String[] pArguments)
 	{
-		RuntimeTest.mRuntimeTest(BFCodexTestSuite.class);	
-		System.out.println("BF code for \"" + pArguments[0] + "\" : " +  BFCodex.mTextToBF(pArguments[0]));
-		
+		//RuntimeTest.mRuntimeTest(BFCodexTestSuite.class);
+		if(pArguments[0].contains("BF"))
+		{
+			System.out.println(BFCodex.mBFToText(pArguments[1]));
+		}
+		else
+		if(pArguments[0].contains("TXT"))
+		{
+			System.out.println(BFCodex.mTextToBF(pArguments[1]));
+		}
+		else
+		{
+			System.out.println("Usage : BFCodex [BF|TXT] [Source|Message]\n");
+		}
 	}
 }
